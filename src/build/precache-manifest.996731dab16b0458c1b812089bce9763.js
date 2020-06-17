da2bf633771d7d49303c65f2fac3754c051d656c
bosmanpa <bsmn.ptrck@gmail.com>
2020-06-16 21:55:10 -0500

    first commit

---
+++ src/build/precache-manifest.996731dab16b0458c1b812089bce9763.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ffc4505f47745e1c8e29abca51f2a12d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d71d5c80f75b96e7a537",
+    "url": "/static/css/main.4a9d094a.chunk.css"
+  },
+  {
+    "revision": "6d451ef0b645672a1d9a",
+    "url": "/static/js/2.ec5bd8c0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.ec5bd8c0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d71d5c80f75b96e7a537",
+    "url": "/static/js/main.32060e3e.chunk.js"
+  },
+  {
+    "revision": "8931283fa7532b66ee7f",
+    "url": "/static/js/runtime-main.fca178c7.js"
+  }
+]);
